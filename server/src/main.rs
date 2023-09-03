019e837d30d198edc5c6604fcd2d954ea027ec75
floweryclover <floweryclover@gmail.com>
2023-09-04 07:47:01 +0900

    ChatServer에 대한 의사코드 작성

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,10 @@
-use tokio::io::AsyncWriteExt;
-
 #[tokio::main]
 async fn main() {
-    let socket = tokio::net::TcpSocket::new_v4().unwrap();
-    socket.bind(std::net::SocketAddr::new(std::net::IpAddr::from([127, 0, 0, 1]), 31410)).unwrap();
 
-    let listener = socket.listen(1024).unwrap();
+    let mut input_buf = String::new();
+    let chat_server = ChatServerBuilder::new()
+        .with_endpoint([127,0,0,1], 31411)
+        .with_passkey_auth(String::from("passkey"));
 
-    let (mut stream, socket_addr) = listener.accept().await.unwrap();
-    let (mut read, mut write) = stream.split();
-    write.write(String::from("안녀엉").as_bytes()).await.unwrap();
+    let _ = chat_server.run_service().await;
 }
