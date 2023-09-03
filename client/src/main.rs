81334aca2d1cd448ec776e7568c8b0d0e0fe61a3
floweryclover <floweryclover@gmail.com>
2023-09-03 22:50:25 +0900

    연결 후 확인용 데이터 송신 구현

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,10 @@
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let mut socket = std::net::TcpStream::connect("127.0.0.1:31410").unwrap();
+    let mut buf: Vec<u8> = Vec::new();
+    buf.resize(1024, 0);
+    let size = socket.read(&mut buf).unwrap();
+    buf.resize(size, 0);
+    println!("{}", String::from_utf8(buf).unwrap());
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::AsyncWriteExt;
+
+#[tokio::main]
+async fn main() {
+    let socket = tokio::net::TcpSocket::new_v4().unwrap();
+    socket.bind(std::net::SocketAddr::new(std::net::IpAddr::from([127, 0, 0, 1]), 31410)).unwrap();
+
+    let listener = socket.listen(1024).unwrap();
+
+    let (mut stream, socket_addr) = listener.accept().await.unwrap();
+    let (mut read, mut write) = stream.split();
+    write.write(String::from("안녀엉").as_bytes()).await.unwrap();
 }
